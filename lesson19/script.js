3cbb3e9a307a59e2e685912786214606c5f2ea62
Tomyum1 <fuitorsil@gmail.com>
2023-02-05 19:55:18 +0700

    sdfghksdghsghsdfgh

---
+++ lesson19/script.js
@@ -0,0 +1,9 @@
+"use strict";
+let hasFinishedEducation = false; 
+const passFinalTest = true;
+ //Если условие верно то изменить переменную hasFinishedEducation на true
+if(passFinalTest) hasFinishedEducation = true; //ошибка в переменной hasFinishedEducation
+ 
+//Если условие верно то вывести текст
+if(hasFinishedEducation) console.log("Поздравляем! Ты закончил обучение!");
+
