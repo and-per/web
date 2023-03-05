ef16ba6b69cb70d57e9456e9a8f7645256096424
MichaelCoolBlood <TsibinMishaPas2010>
2023-03-05 19:57:20 +0700

    asd

---
+++ lesson 23/script.js
@@ -0,0 +1,26 @@
+
+
+// console.log(Math.ceil(5.0000000001)) //3.3 => 4
+
+// console.log(Math.floor(5.999999999)) // 5.99 =>
+
+// console.log(Math.round(5.4))
+
+// console.log(Math.trunc(5.9999999999))
+
+// console.log(5.234.toFixed(2))
+
+
+
+
+
+// let chislo = Math.trunc(Math.random() * 6) + 1;
+// console.log(chislo)
+
+
+// // let inp = document.querySelector("Value")
+// // inp.value = "aa"
+
+// function a() {
+//     document.querySelector(".myText").value = "AAAARGH"
+// }
